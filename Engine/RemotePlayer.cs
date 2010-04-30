542e6804f503d71015d23d75b551ad49894c31d8
David Stern <vorporeal@gmail.com>
2010-04-30 07:32:41 -0400

    Change RemotePlayer so that it draws itself independent of camera type.  (Of course...  copy-paste errors.)

--- Engine/RemotePlayer.cs
+++ Engine/RemotePlayer.cs
@@ -44,11 +44,8 @@ namespace Mammoth.Engine
             base.Draw(gameTime);
 
             IRenderService r = (IRenderService)this.Game.Services.GetService(typeof(IRenderService));
-            ICameraService cam = (ICameraService)this.Game.Services.GetService(typeof(ICameraService));
 
-            // If you're using the first-person camera, don't draw your own geometry.
-            if (cam.Type != Camera.CameraType.FIRST_PERSON)
-                r.DrawRenderable(this);
+            r.DrawRenderable(this);
         }
 
         public override string getObjectType()
