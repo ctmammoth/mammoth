6bced719e9f3d1ecf7ceb065e5d26f90b3441ad8
acoogan <mogget03@gmail.com>
2010-05-04 00:43:50 -0400

    Lulz

--- Engine/ObjectFactories.cs
+++ Engine/ObjectFactories.cs
@@ -7,13 +7,15 @@ namespace Mammoth.Engine
 {
     class ObjectFactories
     {
-        public static Crate makeCrate(double x, double y, double z, double w, double h, double l)
+        /*public static Crate makeCrate(double x, double y, double z, double w, double h, double l)
         {
             String parameters = "";
             parameters = parameters + "x:" + x + " y:" + y + "z: " + z + " w:" + w + " h:" + h + " l:" + l;
 
             XmlHandler.CreateFromXml("Crate", parameters);
-        }
+
+            throw new NotImplementedException();
+        }*/
 
     }
 }
