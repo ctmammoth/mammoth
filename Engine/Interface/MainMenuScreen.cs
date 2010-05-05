a9c89ac87856225cca3b7ca7379285ea14be02fb
David Stern <vorporeal@gmail.com>
2010-05-04 22:08:54 -0400

    Starting to implement the whole interface framework.

---
+++ Engine/Interface/MainMenuScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mammoth.Engine.Interface
+{
+    public class MainMenuScreen : TScreen
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
--- Engine/Interface/TScreenManager.cs
+++ Engine/Interface/TScreenManager.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 using Microsoft.Xna.Framework;
 
+using Mammoth.Engine.Input;
+using Mammoth.Engine.Interface;
+
 namespace Mammoth.Engine.Interface
 {
     public class TScreenManager : DrawableGameComponent
@@ -12,13 +15,143 @@ namespace Mammoth.Engine.Interface
         #region Fields
 
         List<TScreen> _screens = new List<TScreen>();
+        Stack<TScreen> _screensToUpdate = new Stack<TScreen>();
+
+        bool isInitialized = false;
 
         #endregion
 
         public TScreenManager(Game game)
             : base(game)
         {
-
+            
         }
+
+        /// <summary>
+        /// Initialize all of the screens.
+        /// </summary>
+        public override void Initialize()
+        {
+            isInitialized = true;
+
+            foreach (TScreen screen in _screens)
+            {
+                screen.Initialize();
+            }
+        }
+
+        /// <summary>
+        /// Have all screens load any content that they need.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            if(isInitialized)
+                foreach (TScreen screen in _screens)
+                    screen.LoadContent();
+        }
+
+        /// <summary>
+        /// Tells all screens to update and lets them know if they need to handle input.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            // Get the input service.
+            IInputService input = (IInputService)this.Game.Services.GetService(typeof(IInputService));
+
+            // Empty the list of screens to update.
+            _screensToUpdate.Clear();
+
+            // Add each screen to the end of the list of ones to update.
+            foreach(TScreen screen in _screens)
+                _screensToUpdate.Push(screen);
+
+            // Keep track of whether screens have focus (and should handle input).  If the game isn't active, all
+            // screens are out of focus.
+            bool hasFocus = !this.Game.IsActive;
+            // Keep track of whether screens are visible or not.  The topmost screen is visible, so we initialize
+            // this to true.
+            bool visible = true;
+
+            // Loop through the list of screens.
+            while (_screensToUpdate.Count > 0)
+            {
+                // Pop the topmost screen.
+                TScreen screen = _screensToUpdate.Pop();
+
+                // Have the screen update its state.
+                screen.Update(gameTime, hasFocus, visible);
+
+                // If the screen is shown on-screen...
+                if (screen.ScreenState == ScreenState.Active ||
+                    screen.ScreenState == ScreenState.TransitionOn)
+                {
+                    // If this is the top-most screen, have it handle input.
+                    if (hasFocus)
+                    {
+                        screen.HandleInput();
+
+                        // Make sure that no other screens get input events.
+                        hasFocus = false;
+                    }
+
+                    // If this screen is active and not a popup, let the rest
+                    // of the screens know that they are covered up.
+                    if (!screen.IsPopup)
+                        visible = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draw all of the screens managed by this screen manager.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (TScreen screen in _screens)
+                if (screen.ScreenState != ScreenState.Hidden)
+                    screen.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Adds a screen to the screen manager.
+        /// </summary>
+        /// <param name="screen">The screen to add.</param>
+        public void AddScreen(TScreen screen)
+        {
+            // Set the screen manager for the screen.
+            screen.ScreenManager = this;
+            
+            // Have the screen initialize itself and load its content.
+            if (isInitialized)
+            {
+                screen.Initialize();
+                screen.LoadContent();
+            }
+
+            // Add the screen to the list.
+            _screens.Add(screen);
+        }
+
+        /// <summary>
+        /// Removes a screen from this screen manager.
+        /// </summary>
+        /// <param name="screen">The screen to remove.</param>
+        public void RemoveScreen(TScreen screen)
+        {
+            // Have the screen unload its content.
+            // TODO: Add this, if necessary.
+
+            _screens.Remove(screen);
+        }
+
+        #region Properties
+
+        public TScreen[] Screens
+        {
+            get { return _screens.ToArray(); }
+        }
+
+        #endregion
     }
 }
