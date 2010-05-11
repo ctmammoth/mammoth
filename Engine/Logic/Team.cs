227a542aed20a29a770a6ceef34ce5fe1e2e99f1
jplace <tapman90@gmail.com>
2010-05-11 16:47:49 -0400

    YourTeam is now a team

--- Engine/Logic/Team.cs
+++ Engine/Logic/Team.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Collections;
 
+using Mammoth.Engine.Networking;
+
 using Microsoft.Xna.Framework;
 
 namespace Mammoth.Engine
 {
-    public class Team
+    public class Team : IEncodable
     {
         private int TeamID;
         private List<int> MemberIDs;
@@ -106,6 +108,29 @@ namespace Mammoth.Engine
         public string ToString()
         {
             return "Team " + TeamID;
+        }
+
+        public byte[] Encode()
+        {
+            Mammoth.Engine.Networking.Encoder e = new Mammoth.Engine.Networking.Encoder();
+
+            e.AddElement("TeamID", TeamID);
+            e.AddElement("NumKills", NumKills);
+            e.AddElement("NumCaptures", NumCaptures);
+            e.AddElement("SpawnLocation", SpawnLocation);
+
+            return e.Serialize();
+        }
+
+        public void Decode(byte[] serialized)
+        {
+            Mammoth.Engine.Networking.Encoder e = new Mammoth.Engine.Networking.Encoder(serialized);
+
+            TeamID = (int)e.GetElement("TeamID", TeamID);
+            NumKills = (int)e.GetElement("NumKills", NumKills);
+            NumCaptures = (int)e.GetElement("NumCaptures", NumCaptures);
+            SpawnLocation = (Vector3)e.GetElement("SpawnLocation", SpawnLocation);
+
         }
 
     }
