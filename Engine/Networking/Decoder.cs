86e57d6592a8b17885f57125a5b80d597014dab3
pciullo <peter_ciullo@brown.edu>
2010-05-10 21:15:29 -0400

    no decoding rsos

--- Engine/Networking/Decoder.cs
+++ Engine/Networking/Decoder.cs
@@ -122,11 +122,12 @@ namespace Mammoth.Engine.Networking
                     r.Decode(properties);
                 break;
 
+                /*
                 case "RealStaticObject":
                 Console.WriteLine("RSO recieved");
                 RealStaticObject rso = new RealStaticObject(this.game);
                 rso.Decode(properties);
-                break;
+                break;*/
 
                 default:
                 Console.WriteLine("Object type was not recognized: " + type);
